19e0accb9efb3eb159bdf04c79e350bbb7924ffc
illia <illia@extremepush.com>
2020-05-18 14:24:25 +0300

    adding covertion

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const request = require('request');
 const HTMLParser = require('node-html-parser');
 const fs = require('fs');
 const moment = require('moment');
+const ffmpeg = require('fluent-ffmpeg');
+const hrstart = process.hrtime();
 
 
 const BASE_URL = "https://2ch.hk";
@@ -17,7 +19,8 @@ const threadId = myArgs[0];
 
 const mediaLinks = [];
 // create folder for memes
-const date = moment().format('DD-MM-yyyy-HH-mm');
+const time = moment().format('HH:mm:ss')
+const date = moment().format('YYYY-MM-DD');
 console.log(`Downloading memes for ${date}`);
 const memeFolder = `./${date}`;
 if (!fs.existsSync(memeFolder)) {
@@ -25,30 +28,48 @@ if (!fs.existsSync(memeFolder)) {
 }
 
 
-request(`${BASE_URL}${threadId}`, function (error, response, body) {
+request(`${BASE_URL}${threadId}`, async function (error, response, body) {
     var root = HTMLParser.parse(body);
     var links = root.querySelectorAll(linkSelector);
     links.forEach(link => {
         mediaLinks.push(link.getAttribute('href'));
     });
-    console.log(`Found ${mediaLinks.length} media files`)
-    Promise.all(mediaLinks.map(link => {
+    console.log(`Found ${mediaLinks.length} media files`);
+    await Promise.all(mediaLinks.map(link => {
         return new Promise((resolve, reject) => {
-            let file = fs.createWriteStream(`${memeFolder}/${link.slice(17)}`);
-            let stream = request(BASE_URL + link)
+            let fileName = link.slice(17);
+            let filePath = `${memeFolder}/${fileName}`;
+            let file = fs.createWriteStream(filePath);
+            request(BASE_URL + link)
             .pipe(file)
             .on('finish', () => {
-                console.log("\x1b[32m%s\x1b[0m", `File ${link.slice(17)} was downloaded`);
+                console.log("\x1b[32m%s\x1b[0m", `File ${fileName} was downloaded`);
                 resolve();
             })
             .on('error', (error) =>{
                 console.log(error);
             })
         })
-        .catch(error => {
-            console.log(`Something really weird happened: ${error}`);
-        });
     }))
-    .then(res => console.log(`${mediaLinks.length} media files downloaded successfully`),
-    err => console.log(err));
+    // .then(res => {
+    //     fs.readdir(memeFolder, (err, files) => {
+    //         files.forEach(file => {
+    //             if(file.slice(-4) == 'webm') {
+    //                 ffmpeg(`${memeFolder}/${file}`).output(`${memeFolder}/${file.slice(0, -5)}.mp4`)
+    //                     .on('error', (error) => {
+    //                         console.log(`Something really weird happened: ${error}`);
+    //                     })
+    //                     .on('end', () => {
+    //                         console.log(`Converted ${file} into ${file.slice(0, -5)}.mp4`);
+    //                         fs.unlinkSync(`${memeFolder}/${file}`);
+    //                         console.log(`Removed file ${file}`);
+    //                     })
+    //                     .run();
+    //             }
+    //         });
+    //     });
+    // })
+    .then(() => {
+        console.log("Finished after %ds", (process.hrtime(hrstart))[0]);
+    });
 });
--- test.js
+++ test.js
@@ -3,8 +3,63 @@ const fs = require('fs');
 const moment = require('moment');
 const HTMLParser = require('node-html-parser');
 
-request("https://2ch.hk/b/catalog.html", function(err, response, body) {
-    var root = HTMLParser.parse(body);
-    var threads = root.querySelectorAll('div.thread_wrap');
-    console.log(body);
+const ffmpeg = require('fluent-ffmpeg');
+
+
+const memeFolder = `2020-05-18`;
+fs.readdir(memeFolder, async (err, files) => {
+    const webms = files.filter((file) => file.slice(-4) == 'webm');
+    const chunked_webms = chunk(webms, 3);
+    for (const chunk of chunked_webms) {
+        for(const webm of chunk) {
+            await convert(webm).catch(err => console.log(err));
+        }
+    }
 });
+
+function chunk(array, size) {
+    const chunked_arr = [];
+    let copied = [...array]; // ES6 destructuring
+    const numOfChild = Math.ceil(copied.length / size); // Round up to the nearest integer
+    for (let i = 0; i < numOfChild; i++) {
+      chunked_arr.push(copied.splice(0, size));
+    }
+    return chunked_arr;
+  }
+
+
+async function convert(file) {
+    return new Promise((resolve, reject) => {
+        console.log(`Converting ${file} into ${file.slice(0, -5)}.mp4`);
+        ffmpeg(`${memeFolder}/${file}`).output(`${memeFolder}/${file.slice(0, -5)}.mp4`)
+            .on('error', (error) => {
+                console.log(`Something really weird happened: ${error}`);
+                reject(error);
+            })
+            .on('end', () => {
+                console.log(`Converted ${file} into ${file.slice(0, -5)}.mp4`);
+                fs.unlinkSync(`${memeFolder}/${file}`);
+                console.log(`Removed file ${file}`);
+                resolve();
+            })
+            .run();
+        });
+}
+// async function convertVideo (file) {
+//     ffmpeg(`${memeFolder}/${file}`).output(`${memeFolder}/${file.slice(0, -5)}.mp4`)
+//                 .on('error', (error) => {
+//                     console.log(`Something really weird happened: ${error}`);
+//                 })
+//                 .on('end', () => {
+//                     console.log(`Converted ${file} into ${file.slice(0, -5)}.mp4`);
+//                     fs.unlinkSync(`${memeFolder}/${file}`);
+//                     console.log(`Removed file ${file}`);
+//                 })
+//                 .run();
+//     return;                
+// }
+
+
+
+
+// convert.addInput('15891073901460.webm').output('out.mp4').run();
