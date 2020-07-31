5de812e6eb985abd6fab3ecf70503a136435c95b
illia <illia@extremepush.com>
2020-07-31 14:05:49 +0300

    adding waits for conversion and changing to handbrake instead of ffmpeg"

--- convert.js
+++ convert.js
@@ -12,6 +12,13 @@ const {
     clearFailedLog,
     checkFileSize
     } = require('./2chClient.js');
+const { resolve, reject } = require('bluebird');
+const hbjs = require('handbrake-js');
+const request = require('request');
+const path = require('path');
+const WARN_COLOR = "\x1b[33m%s\x1b[0m";
+const ERR_COLOR = "\x1b[31m%s\x1b[0m";
+const GOOD_COLOR = "\x1b[32m%s\x1b[0m";
 
 function checkEmpty(filePath) {
     let fileSize = fs.statSync(filePath).size;
@@ -27,24 +34,68 @@ module.exports = {
      * 
      * @return {string} filePath
      */
+    // convert: (filePath, fileName) => {
+    //     return new Promise(async (resolve, reject) => {
+    //         console.log(`Converting file ${fileName}`)
+    //         const newFileName = `${fileName.slice(0, -5)}.mp4`;
+    //         const newFilePath = `${filePath.slice(0, -5)}.mp4`;
+    //         ffmpeg(filePath).output(`${filePath.slice(0,-5)}.mp4`)
+    //             .on('error', (error) => {
+    //                 logFailure(filePath, error.message.replace(/[^a-zA-Z0-9]/g, ""));
+    //                 return reject(error.message)
+    //             })
+    //             .on('end', () => {
+    //                 console.log(`Converted ${fileName} into ${newFileName}`);
+    //                 fs.unlinkSync(filePath);
+    //                 console.log(`Removed file ${fileName}`);
+    //                 return resolve(newFilePath);
+    //             })
+    //             .run();
+    //     }).catch(err => console.log(err));
+    // },
     convert: (filePath, fileName) => {
-        return new Promise((resolve, reject) => {
-            console.log(`Converting file ${fileName}`)
+        return new Promise(async (resolve, reject) => {
             const newFileName = `${fileName.slice(0, -5)}.mp4`;
             const newFilePath = `${filePath.slice(0, -5)}.mp4`;
-            ffmpeg(filePath).output(`${filePath.slice(0,-5)}.mp4`)
-                .on('error', (error) => {
-                    // TODO
-                    // logFailure(filePath, error.replace(/[^a-zA-Z0-9]/g, ""));
-                    return reject(error.message)
+            console.log(filePath);
+            await hbjs.spawn({ input: filePath, output: `${filePath.slice(0, -5)}.mp4` })
+                .on('error', err => {
+                    console.log(ERR_COLOR, err.message);
+                    return reject(err);
                 })
                 .on('end', () => {
-                    console.log(`Converted ${fileName} into ${newFileName}`);
+                    console.log(GOOD_COLOR, `Converted ${fileName} into ${newFileName}`);
                     fs.unlinkSync(filePath);
-                    console.log(`Removed file ${fileName}`);
+                    console.log(GOOD_COLOR, `Removed file ${fileName}`);
+                    return resolve(newFilePath);
                 })
-                .run();
-                return resolve(newFilePath);
-            }).catch(err => console.log(err));
+        })
     },
+    checkExistsWithTimeout: (filePath, timeout = 5000) => {
+        return new Promise(function (resolve, reject) {
+            var timer = setTimeout(function () {
+                watcher.close();
+                reject(new Error('File did not exists and was not created during the timeout.'));
+            }, timeout);
+    
+            fs.access(filePath, fs.constants.R_OK, function (err) {
+                if (!err) {
+                    clearTimeout(timer);
+                    watcher.close();
+                    resolve(filePath);
+                }
+            });
+    
+            var dir = path.dirname(filePath);
+            var basename = path.basename(filePath);
+            var watcher = fs.watch(dir, function (eventType, filename) {
+                if (eventType === 'rename' && filename === basename) {
+                    clearTimeout(timer);
+                    watcher.close();
+                    console.log('resolving')
+                    resolve(filePath);
+                }
+            });
+        });
+    }
 };
--- test.js
+++ test.js
@@ -5,62 +5,39 @@ const threadLinkSelector = "div.pager a";
 const request = require('request-promise');
 const HTMLParser = require('node-html-parser');
 // const fs = require('fs');
+const {
+  saveLinks,
+  getMediaLinks, 
+  getThreadLinks, 
+  filterLinks, 
+  downloadMemes, 
+  getFailedVideos, 
+  logFailure,
+  clearFailedLog,
+  checkFileSize,
+  saveThreads,
+  } = require('./2chClient.js');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const moment = require('moment');
 const ffmpeg = require('fluent-ffmpeg');
 const hrstart = process.hrtime();
+const _ = require('lodash');
 const csv = require('csv-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = require('csv-write-stream');
 const { link } = require('fs');
 const threadIdsCsvPath = __dirname + '/threadIds.csv';
+const CHANNEL = require('./channelIds.js');
+const TOKEN = CHANNEL.token;
+const {convert,checkExistsWithTimeout} = require('./convert.js');
+const CHANNEL_ID = CHANNEL.test;
+const Telegram = require('telegraf/telegram');
+const { errorMonitor } = require('stream');
+const { resolve, reject } = require('bluebird');
+const telegram = new Telegram(TOKEN);
+const caption = '[Толстый движ](https://t.me/joinchat/AAAAAEhqKmKjMfH9YIR85w)';
+const WARN_COLOR = "\x1b[33m%s\x1b[0m";
+const ERR_COLOR = "\x1b[31m%s\x1b[0m";
+const GOOD_COLOR = "\x1b[32m%s\x1b[0m";
 
-
-function getThreadLinks() {
-  return new Promise((resolve, reject) => request(ARCH)
-      .then(res => {
-          let root = HTMLParser.parse(res);
-          let links = [];
-          root.querySelectorAll(threadLinkSelector).forEach(link => links.push(link.getAttribute('href')));
-          return links[links.length - 2];
-      })
-      .then((archLink) => request(BASE_URL + archLink))
-      .then(res => {
-          let root = HTMLParser.parse(res);
-          let threads = root.querySelectorAll(".box-data a");
-          let threadLinks = Array.from(threads).filter(link => link.text.toLowerCase().includes("webm")).map(a => a.getAttribute("href"));
-          return resolve(threadLinks);
-      })
-      .catch(error => reject(error)))
-}
-
-function checkLinks(links) {
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(threadIdsCsvPath)
-      .pipe(csv())
-      .on('data', row => {
-        links.map(link => {
-          if(row.threadId == link) {
-            links.splice(links.indexOf(link), 1);
-          }
-        })
-      })
-      .on('end', () => {
-        return resolve(links);
-      })
-      .on('error', (error) => reject(error));
-  })
-}
-
-function saveLinks(links) {
-  new Promise((resolve, reject) => {
-    let writer = csvWriter({sendHeaders: fs.readFileSync(threadIdsCsvPath).length === 0});
-    writer.pipe(fs.createWriteStream(threadIdsCsvPath, {flags: 'a'}));
-    links.forEach(link => writer.write({threadId: link}))
-    writer.end();
-    return resolve(links);
-  })
-}
-
-getThreadLinks().then(checkLinks).then(saveLinks)
