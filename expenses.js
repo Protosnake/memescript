e55a6fe77b750d19a27caaec4040ec9723199980
illia <illia@extremepush.com>
2021-01-25 00:41:32 +0200

    25-01-21

--- expenses.js
+++ expenses.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const moment = require('moment');
 const csv = require('csv-parser');
 const csvWriter = require('csv-write-stream');
-const path = __dirname + '/expenses.csv';
+const { exception } = require('console');
+// const path = __dirname + '/expenses.csv';
 
 function run() {
+    let path = process.argv.slice(2)[0];
+    if (!path) throw new exception("No CSV passed");
     let investment = 0;
     let ad_revenue = 0;
     let total_expenses = 0;
@@ -17,10 +20,10 @@ function run() {
         }
     })
     .on('end', () => {
-        console.log(`Расходы за месяц ${total_expenses} руб`)
-        console.log(`Доходы от рекламы за месяц составили ${ad_revenue} руб`)
-        console.log(`Инвестировано за месяц ${investment} руб`);
-        console.log(`Прибыль за месяц: ${ad_revenue - total_expenses - investment}`)
+        console.log(`Расходы за месяц: ${total_expenses} руб`)
+        console.log(`Доходы от рекламы за месяц составили: ${ad_revenue} руб`)
+        console.log(`Инвестировано за месяц: ${investment} руб`);
+        console.log(`Прибыль за месяц: ${ad_revenue - total_expenses}`)
     })
     .on('error', error => console.log(error));
 }
