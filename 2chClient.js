2056418b0b40b47ebbed1b778db5e0536624009f
illia <illia@extremepush.com>
2021-03-30 17:09:02 +0300

    30-08-2021

--- 2chClient.js
+++ 2chClient.js
@@ -48,7 +48,7 @@ module.exports = {
                 let links = [];
                 root.querySelectorAll(threadLinkSelector).forEach(link => links.push(link.getAttribute('href')));
                 // return links.slice(links.length - 6, -1);
-                return links.slice(links.length - 6, -1);
+                return links.slice(-1);
             })
             .then(async archLinks => {
                 let links = [];
