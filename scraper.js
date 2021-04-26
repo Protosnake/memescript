8d17c4aa2be95b46b0e18c900e3bf57003711296
Ilya <illiashyrokykh@gmail.com>
2021-04-26 14:02:59 +0300

    finished scraper with filters

--- scraper.js
+++ scraper.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const ObjectsToCsv = require('objects-to-csv');
-
+const fs = require('fs');
 
 async function run() {
     const emailSelector = '#edit-name';
@@ -41,45 +41,70 @@ async function run() {
     await page.click(submitButtonSelector);
     
     await page.waitForSelector(selectors.candidate);
-    let currentUrl = await page.url();
-    let offset = 1641;
 
-    let pagesNum = 22195 - offset;
-
-
-    let people = []
-    
-    for (const i of Array(pagesNum).keys()) {
-        await page.goto(`${currentUrl}&page=${i + offset}`, {timeout: 300000});
-        // 5D=skill%3AAndroid&f%5B%5D=skill%3AC&f%5B%5D=skill%3AC%23&f%5B%5D=skill%3AC%2B%2B&f%5B%5D=skill%3AGolang&f%5B%5D=skill%3AiOS&f%5B%5D=skill%3AJava&f%5B%5D=skill%3AJavaScript&f%5B%5D=skill%3ANode.js&f%5B%5D=skill%3AQA+Engineering&f%5B%5D=skill%3AReact+Native&f%5B%5D=skill%3AReact.js&f%5B%5D=skill%3ARuby&f%5B%5D=skill%3ARuby+on+Rails&f%5B%5D=skill%3AUnity
-        // await newPage.goto(`https://app.turbohiring.co/uk/front?recruiter=0&loc%5B0%5D=102&page=${i + offset}`, {timeout: 300000});
-        await page.waitForSelector(selectors.candidate, {timeout: 300000});
-        let elNum = (Array.from(await page.$$(selectors.candidate)).length);
-        for (const n of Array(elNum).keys()) {
-
-            let candidateData = {}
-            let candidateElement = await page.$(`.views-row:nth-child(${n + 1})`);
-            candidateData['name'] = await candidateElement.$eval(selectors.name, el => el.textContent.trim());
-            // candidateData['experience'] = await candidateElement.$eval(selectors.experience, el => el.textContent.trim().replace(/(\r\n|\n|\r)/gm, "").trim());
-            candidateData['experience'] = await candidateElement.$(selectors.experience) ? await candidateElement.$eval(selectors.experience, el => el.textContent.trim().replace(/(\r\n|\n|\r)/gm, "").trim()) : undefined;
-            candidateData['skills'] = await candidateElement.$(selectors.skills) ? await candidateElement.$eval(selectors.skills, el => el.textContent.trim().replace(/(\r\n|\n|\r)/gm, "")) : undefined;
-            candidateData['email'] = await candidateElement.$(selectors.emailIcon) ? await candidateElement.$eval(selectors.emailIcon, (el, dataAttribute) => el.parentElement.getAttribute(dataAttribute), selectors.dataAttribute) : undefined;
-            candidateData['phone'] = await candidateElement.$(selectors.phoneIcon) ? await candidateElement.$eval(selectors.phoneIcon, (el, dataAttribute) => el.parentElement.getAttribute(dataAttribute), selectors.dataAttribute) : undefined;
-            candidateData['skype'] = await candidateElement.$(selectors.skypeIcon) ? await candidateElement.$eval(selectors.skypeIcon, (el, dataAttribute) => el.parentElement.getAttribute(dataAttribute), selectors.dataAttribute) : undefined; 
-            candidateData['facebook'] = await candidateElement.$(selectors.facebookIcon) ? await candidateElement.$eval(selectors.facebookIcon, el => el.parentElement.getAttribute('title')) : undefined;
-            candidateData['telegram'] = await candidateElement.$(selectors.telegramIcon) ? await candidateElement.$eval(selectors.telegramIcon, el => el.parentElement.getAttribute('title')) : undefined;
-            candidateData['linkedin'] = await candidateElement.$(selectors.linkedInIcon) ? await candidateElement.$eval(selectors.linkedInIcon, el => {
-                let link = el.parentElement.getAttribute('href');
-                return `https://${link.slice(link.search(/www/)).replace(/%252F/gi, '/')}`
-            }) : undefined;
-            
-            // new ObjectsToCsv([candidateData]).toDisk('./people.csv', {append: true});
-            
-            // people.push(candidateData);
-
-        }
-        console.log(`Done with https://app.turbohiring.co/uk/front?page=${i + offset}`);
+    let filters = {
+        // 'android': 'f%5B%5D=skill%3AAndroid',
+        // 'C': 'f%5B%5D=skill%3AC',
+        'C#': 'f%5B%5D=skill%3AC%23',
+        'C++': 'f%5B%5D=skill%3AC%2B%2B',
+        'GOlang': 'f%5B%5D=skill%3AGolang',
+        'iOS': 'f%5B%5D=skill%3AiOS',
+        'Java': 'f%5B%5D=skill%3AJava',
+        'JavaScript': 'f%5B%5D=skill%3AJavaScript',
+        'NodeJS': 'f%5B%5D=skill%3ANode.js',
+        'AQA': 'f%5B%5D=skill%3AQA+Engineering',
+        'React Native': 'f%5B%5D=skill%3AReact+Native',
+        'React JS': 'f%5B%5D=skill%3AReact.js',
+        'Ruby': 'f%5B%5D=skill%3ARuby',
+        'Ruby on Rail': 'f%5B%5D=skill%3ARuby+on+Rails',
+        'Unity': 'f%5B%5D=skill%3AUnity',
     }
+    
+    for (const filter in filters) {
+        await page.goto(`https://app.turbohiring.co/uk/front?${filters[filter]}`, {timeout: 300000});
+        let pagesNum;
+        try {
+            let lastPageElementLink = await page.$eval('.pager-last a', el => el.getAttribute('href'));
+            pagesNum = lastPageElementLink.slice(lastPageElementLink.search('page') + 5);
+        } catch(err) {
+            pagesNum = 1;
+        }
+        fs.truncate(`./${filter}.csv`, 0, () => console.log(`Cleared ./${filter}.csv`));
+        for (const i of Array(Number(pagesNum)).keys()) {
+            let pageUrl = `https://app.turbohiring.co/uk/front?${filters[filter]}&page=${i}`;
+            await page.goto(pageUrl, {timeout: 300000});
+            // 5D=skill%3AAndroid&f%5B%5D=skill%3AC&f%5B%5D=skill%3AC%23&f%5B%5D=skill%3AC%2B%2B&f%5B%5D=skill%3AGolang&f%5B%5D=skill%3AiOS&f%5B%5D=skill%3AJava&f%5B%5D=skill%3AJavaScript&f%5B%5D=skill%3ANode.js&f%5B%5D=skill%3AQA+Engineering&f%5B%5D=skill%3AReact+Native&f%5B%5D=skill%3AReact.js&f%5B%5D=skill%3ARuby&f%5B%5D=skill%3ARuby+on+Rails&f%5B%5D=skill%3AUnity
+            await page.waitForSelector(selectors.candidate, {timeout: 300000});
+            let elNum = (Array.from(await page.$$(selectors.candidate)).length);
+            for (const n of Array(elNum).keys()) {
+                let candidateData = {}
+                let candidateElement = await page.$(`.views-row:nth-child(${n + 1})`);
+                candidateData['name'] = await candidateElement.$eval(selectors.name, el => el.textContent.trim());
+                // candidateData['experience'] = await candidateElement.$eval(selectors.experience, el => el.textContent.trim().replace(/(\r\n|\n|\r)/gm, "").trim());
+                candidateData['experience'] = await candidateElement.$(selectors.experience) ? await candidateElement.$eval(selectors.experience, el => el.textContent.trim().replace(/(\r\n|\n|\r)/gm, "").trim()) : undefined;
+                candidateData['skills'] = await candidateElement.$(selectors.skills) ? await candidateElement.$eval(selectors.skills, el => el.textContent.trim().replace(/(\r\n|\n|\r)/gm, "")) : undefined;
+                candidateData['email'] = await candidateElement.$(selectors.emailIcon) ? await candidateElement.$eval(selectors.emailIcon, (el, dataAttribute) => el.parentElement.getAttribute(dataAttribute), selectors.dataAttribute) : undefined;
+                candidateData['phone'] = await candidateElement.$(selectors.phoneIcon) ? await candidateElement.$eval(selectors.phoneIcon, (el, dataAttribute) => el.parentElement.getAttribute(dataAttribute), selectors.dataAttribute) : undefined;
+                candidateData['skype'] = await candidateElement.$(selectors.skypeIcon) ? await candidateElement.$eval(selectors.skypeIcon, (el, dataAttribute) => el.parentElement.getAttribute(dataAttribute), selectors.dataAttribute) : undefined; 
+                candidateData['facebook'] = await candidateElement.$(selectors.facebookIcon) ? await candidateElement.$eval(selectors.facebookIcon, el => el.parentElement.getAttribute('title')) : undefined;
+                candidateData['telegram'] = await candidateElement.$(selectors.telegramIcon) ? await candidateElement.$eval(selectors.telegramIcon, el => el.parentElement.getAttribute('title')) : undefined;
+                candidateData['linkedin'] = await candidateElement.$(selectors.linkedInIcon) ? await candidateElement.$eval(selectors.linkedInIcon, el => {
+                    let link = el.parentElement.getAttribute('href');
+                    return `https://${link.slice(link.search(/www/)).replace(/%252F/gi, '/')}`
+                }) : undefined;
+                
+                new ObjectsToCsv([candidateData]).toDisk(`./${filter}.csv`, {append: true});
+                
+                // people.push(candidateData);
+    
+            }
+            console.log(`Done with ${await page.url()}`);
+        }
+        console.log(`Done with ${filter}`);
+    }
+
+
+
     
     // const csv = new ObjectsToCsv(people);
     // await csv.toDisk('./people.csv');
